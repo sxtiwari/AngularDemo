027912463ac449047132ac14eec4a129f73771ef
sxtiwari <saurabh.tiwari@kony.com>
2015-03-29 10:47:27 +0530

    bug fix

--- src/client/app/components/home/indexController.js
+++ src/client/app/components/home/indexController.js
@@ -10,7 +10,6 @@ var indexController = function(scopeVar, clientId){
             // Update the app to reflect a signed in user
             // Hide the sign-in button now that the user is authorized, for example:
             document.getElementById('signinButton').setAttribute('style', 'display: none');
-            debug;
             console.log("You Are Signed In");
         } else {
             // Update the app to reflect a signed out user
@@ -18,10 +17,30 @@ var indexController = function(scopeVar, clientId){
             //   "user_signed_out" - User is signed-out
             //   "access_denied" - User denied access to your app
             //   "immediate_failed" - Could not automatically log in the user
-            debug;
             console.log('Sign-in state: ' + authResult['error']);
         }
     }
+
+    scopeVar.renderSignInButton = function() {
+        gapi.signin.render('signinButton',
+            {
+                'callback': scopeVar.googleSignInCallback, // Function handling the callback.
+                'clientid': clientId, // CLIENT_ID from developer console which has been explained earlier.
+                'requestvisibleactions': 'http://schemas.google.com/AddActivity', // Visible actions, scope and cookie policy wont be described now,
+                // as their explanation is available in Google+ API Documentation.
+                'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email',
+                'cookiepolicy': 'single_host_origin'
+            }
+        );
+    }
+
+    // Start function in this example only renders the sign in button.
+    scopeVar.start = function() {
+        scopeVar.renderSignInButton();
+    };
+
+    // Call start function on load.
+    scopeVar.start();
 };
 indexController.$inject = ['$scope', 'GOOGLE_CLIENT_ID'];
 angular.module('sso').controller('indexController', indexController);
